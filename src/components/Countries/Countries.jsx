069d58104e4230a41206c7b0a21b554aab0c8347
adnanalemran <adnanalemranontor@gmail.com>
2023-09-11 14:25:01 +0600

    add

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import "./Conutries.css";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
@@ -11,20 +12,20 @@ const Countries = () => {
   }, []);
 
   return (
-    <div>
-      <h3 className="text-2xl font-bold text-center text-red-500 p-8">
-        Countries: {countries.length}
-      </h3>
-  <div className="grid  gap-4 grid-cols-1 lg:grid-cols-4 px-10 border-rose-700">
-      {
-        countries.map((country, index) => (
-          <Country key={index} country={country} />
-        ))
-      }
-
-  </div>
-
-    </div>
+    <>
+      <div className="container mx-auto">
+        <h3 className="text-2xl font-bold text-center text-red-500 p-8">
+          Countries: {countries.length}
+        </h3>
+        <div className="grid grid-cols-3 gap-4">
+          {countries.map((country) => (
+            <Country key={country.cca3} country={country}>
+            
+            </Country>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,30 +1,38 @@
+import { useState } from "react";
+import "./Country.css";
 const Country = ({ country }) => {
-  const { name } = country;
-  const { flags} = country;
+  const { name, flags, population, area, cca3 } = country;
+  const [visited, setVested] = useState(false);
+
+  const handleVisited = () => {
+    setVested(!visited);
+  };
 
   return (
-    <div >
+<> 
+<div className="card bg-base-100 shadow-xl bg-[#061e2e] text-white">
+  <figure className="px-10 pt-10"><img src={flags.png} alt="" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{name.common}</h2>
+     population : {population} 
+       area: {area} 
       
-     
+        <small>Code {cca3}</small>
+    
+    <span className="font-bold text-red-500">
+    {visited  ? ` i have Visited this country  ` : 'i want to visit'}
+    </span>
+    <div className="card-actions justify-end">
+      <button onClick={handleVisited}  className="btn btn-primary">{visited ? 'No Visited' : 'mark as visited'}</button>
 
-      <div className="card card-side bg-base-100 shadow-xl   ">
-        <figure>
-          <img 
-          className='w-28 p-4'
-            src={flags.png}
-            alt="Movie"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Country Name: {name?.common}</h2>
-          <p>Click the button to watch on Jetflix app.</p>
-
-          <div className="card-actions justify-end">
-             
-          </div>
-        </div>
-      </div>
     </div>
+  </div>
+</div>
+
+
+
+ 
+    </>
   );
 };
 
